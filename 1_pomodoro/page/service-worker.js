23ba1b7016ac824123259b33b0dceb9b81c0539b
Askie Lin <isme408@gmail.com>
2019-07-21 01:32:21 +0800

    Rebuild Again

---
+++ 1_pomodoro/page/service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/spirit_2019/1_pomodoro/precache-manifest.63a7437f51fb07bd76d309cc66d10df2.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "1_pomodoro"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- 1_pomodoro/vue.config.js
+++ 1_pomodoro/vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/spirit_2019/1_pomodoro/'
     : '/',
-  outputDir: './view',
+  outputDir: './page',
 };
